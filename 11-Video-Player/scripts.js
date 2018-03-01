b4c3c35921bbd843d23421aecc4ee7933cb7d3cb
guti7 <7505855+guti7@users.noreply.github.com>
2018-03-01 07:26:45 -0800

    Toggle video playback

--- 11-Video-Player/scripts.js
+++ 11-Video-Player/scripts.js
@@ -3,10 +3,26 @@ const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
-const toggle = player.querySelector('.toggle');
+const playerToggleButton = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
 /* Build the functions */
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
+
+function updatePlayerToggle() {
+  console.log(playerToggleButton);
+  const icon = this.paused ? '❚❚' : '►';
+  console.log(`icon: ${icon}`);
+  playerToggleButton.textContent = icon;
+}
 
 /* Hook up event listeners */
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updatePlayerToggle);
+video.addEventListener('pause', updatePlayerToggle);
+
+playerToggleButton.addEventListener('click', togglePlay);
